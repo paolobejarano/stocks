c8b9ad282adfd174ea156d81389f36b607802a1e
Paolo Bejarano <paolo@kurios.la>
2022-06-25 10:06:15 -0500

    Create obtener_portafolio method

--- main.py
+++ main.py
@@ -22,3 +22,25 @@ def get_stocks_prices() -> dict:
         prices[symbol] = stock.get_stock_price()
     return prices
 
+
+def obtener_portafolio(capital: float) -> dict:
+    portafolio = {}
+    prices = get_stocks_prices()
+    for stock in AVAILABLE_STOCKS:
+        portafolio[stock] = 0
+        if capital > prices[stock]:
+            capital -= prices[stock]
+            portafolio[stock] = 1
+        else:
+            break
+    prices = sorted(prices.items(), key=lambda x: x[1], reverse=True)
+    sorted_prices = {}
+    for price in prices:
+        sorted_prices[price[0]] = price[1]
+    for stock in sorted_prices:
+        portafolio[stock] += capital // sorted_prices[stock]
+        capital -= sorted_prices[stock] * (capital // sorted_prices[stock])
+    return portafolio
+
+
+print(obtener_portafolio(capital=3000000))
